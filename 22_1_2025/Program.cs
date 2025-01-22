59f0539b9e6535a04860a2ce7928a114e6f5fec4
Ammar <amar.alomari@gmail.com>
2025-01-22 17:06:30 +0300

    task

--- 22_1_2025/Program.cs
+++ 22_1_2025/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,55 @@ namespace _22_1_2025
 {
     internal class Program
     {
+        class vehicle
+        {
+            public string brand { get; set; }
+            public string model { get; set; }
+
+
+
+            public void print()
+            {
+
+                Console.WriteLine($"the brand : {brand} and the model {model} ");
+            }
+
+           
+        }
+        class car : vehicle
+        {
+            public int numberOfDoor { get; set; }
+
+
+        }
+
+
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("ddd");
+            vehicle vehicle1 = new vehicle();
+            vehicle1.brand = "avanti";
+            vehicle1.model = "hundai";
+
+
+
+            car car1 = new car();
+            car1.brand = "bmw";
+            car1.model = "e46";
+            car1.numberOfDoor = 4;
+            car1.print();
+     
+           
         }
     }
 }
+
+//What is constructor ==> giving inttial value
+//What are the basic concepts of OOPs ==> inhertance encapsolation polymorphisem abstraction
+//What is the Encapsulation ==> get set 500 
+//What is the sealed class ==> didn't inhertance 
+//What is the Inheritance concept ==> give the method and propartis for the child
+
---
+++ prepare/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prepare
+{
+    internal class Program
+    {
+
+        class employee
+        {
+
+            public int empId { get; set; } // auto property  //1
+
+            private double age; // field
+
+            public double Age // property related to specific field 
+            { get {
+                    return age;
+
+                } set
+                {
+
+                    age = value;
+
+
+                } } // property
+
+
+            public employee(int empId , double Age)
+            {
+
+                //property       // value come from main 
+                this.empId = empId;
+                this.Age = Age;
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+                employee e = new employee(1,12);
+              //  e.Age = 10;
+
+
+            Console.WriteLine(e.empId);
+
+
+        }
+    }
+}
